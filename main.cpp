344acb962d43a2da7f70ca670f0250ccb3e5fa2d
deniztohumcu <deniz.tohumcu93@gmail.com>
2019-11-26 06:12:56 +0100

    TODOs

--- main.cpp
+++ main.cpp
@@ -8,3 +8,8 @@ int main(int argc, const char * argv[]) {
     graphs simulation(parsed.outputPerGateMapGiven,parsed.inputPerGateMapGiven,parsed.inputsMapGiven,parsed.outputMapGiven,parsed.adjancencyMapGiven,parsed.nameTypeMapGiven,parsed.gateNumberGiven,parsed.inputNumberGiven,parsed.outputNumberGiven,parsed.simulationSizeGiven);
     return 0;
 }
+
+//TODO1: Better organizing the log functions and gather them in to log.h
+//TODO2: Changing the way (optimizing) the input vectors handling in graphs.h and parser.h
+//TODO3: Optimize the code for scalability
+//TODO4: Make more modular functions
